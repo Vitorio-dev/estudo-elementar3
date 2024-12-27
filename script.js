f98038f9a698ae187f88fdd50a4eb6f60c5ab675
vitorio-dev <mvitorio@almavivasolutions.com.br>
2024-12-27 11:02:44 -0300

    testando a logica de mudança de posição

--- script.js
+++ script.js
@@ -28,22 +28,32 @@
 //   });
 // });
 
+const styleCaixaUm =
+  "position: absolute;margin-left: 450px; margin-top: 300px; border: 3px solid rgb(255, 234, 0);";
+const styleCaixaDois =
+  " position: absolute;margin-left: 300px;margin-top: 200px; border: 3px solid rgb(11, 209, 17);";
+const styleCaixaTres =
+  "  position: absolute;margin-left: 150px;margin-top: 100px;border: 3px solid rgb(247, 0, 119);";
+const styleCaixaQuatro = " border: 3px solid black;position: relative;";
 
-const caixaUm = document.createElement('div');
-caixaUm.classList.add('caixa_um');
+const caixaUm = document.createElement("div");
+caixaUm.classList.add("caixa_um");
+caixaUm.style.cssText = styleCaixaUm;
 
-const caixaDois = document.createElement('div');
-caixaDois.classList.add('caixa_dois');
+const caixaDois = document.createElement("div");
+caixaDois.classList.add("caixa_dois");
+caixaDois.style.cssText = styleCaixaDois;
 
-const caixaTres = document.createElement('div');
-caixaTres.classList.add('caixa_tres');
+const caixaTres = document.createElement("div");
+caixaTres.classList.add("caixa_tres");
+caixaTres.style.cssText = styleCaixaTres;
 
-const caixaQuatro = document.createElement('div');
-caixaQuatro.classList.add('caixa_quatro');
+const caixaQuatro = document.createElement("div");
+caixaQuatro.classList.add("caixa_quatro");
+caixaQuatro.style.cssText = styleCaixaQuatro;
 
 document.body.appendChild(caixaUm);
 document.body.appendChild(caixaDois);
 document.body.appendChild(caixaTres);
 document.body.appendChild(caixaQuatro);
-
 
