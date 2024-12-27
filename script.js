411293576fbec8602dbdbb2d215561a1b173cd92
vitorio-dev <mvitorio@almavivasolutions.com.br>
2024-12-27 09:50:16 -0300

    testando as trocas de posição

---
+++ script.js
@@ -0,0 +1,31 @@
+// const caixas = [
+//   document.querySelector(".caixa_um"),
+//   document.querySelector(".caixa_dois"),
+//   document.querySelector(".caixa_tres"),
+//   document.querySelector(".caixa_quatro"),
+// ];
+
+// function trocarClasses(indice1, indice2) {
+//   const caixa1 = caixas[indice1];
+//   const caixa2 = caixas[indice2];
+
+//   const tempClass = caixa1.className;
+//   caixa1.className = caixa2.className;
+//   caixa2.className = tempClass;
+// }
+
+// caixas.forEach((caixa, index) => {
+//   caixa.addEventListener("click", () => {
+//     if (index === 0) {
+//       trocarClasses(0, 1);
+//     } else if (index === 1) {
+//       trocarClasses(1, 2);
+//     } else if (index === 2) {
+//       trocarClasses(2, 3);
+//     } else if (index === 3) {
+//       trocarClasses(3, 0);
+//     }
+//   });
+// });
+
+
