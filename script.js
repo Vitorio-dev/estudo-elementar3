d894886270aaa13a464ead12ce317370c7d3e5cb
vitorio-dev <mvitorio@almavivasolutions.com.br>
2024-12-27 11:41:39 -0300

    V1 da troca de posição

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
-// const caixas = [
-//   document.querySelector(".caixa_um"),
-//   document.querySelector(".caixa_dois"),
-//   document.querySelector(".caixa_tres"),
-//   document.querySelector(".caixa_quatro"),
-// ];
-
-// function trocarClasses(indice1, indice2) {
-//   const caixa1 = caixas[indice1];
-//   const caixa2 = caixas[indice2];
-
-//   const tempClass = caixa1.className;
-//   caixa1.className = caixa2.className;
-//   caixa2.className = tempClass;
-// }
-
-// caixas.forEach((caixa, index) => {
-//   caixa.addEventListener("click", () => {
-//     if (index === 0) {
-//       trocarClasses(0, 1);
-//     } else if (index === 1) {
-//       trocarClasses(1, 2);
-//     } else if (index === 2) {
-//       trocarClasses(2, 3);
-//     } else if (index === 3) {
-//       trocarClasses(3, 0);
-//     }
-//   });
-// });
 
 const styleCaixaUm =
-  "position: absolute;margin-left: 450px; margin-top: 300px; border: 3px solid rgb(255, 234, 0);";
+  "position: absolute;margin-left: 450px; margin-top: 300px;";
 const styleCaixaDois =
-  " position: absolute;margin-left: 300px;margin-top: 200px; border: 3px solid rgb(11, 209, 17);";
+  " position: absolute;margin-left: 300px;margin-top: 200px;";
 const styleCaixaTres =
-  "  position: absolute;margin-left: 150px;margin-top: 100px;border: 3px solid rgb(247, 0, 119);";
-const styleCaixaQuatro = " border: 3px solid black;position: relative;";
+  " position: absolute;margin-left: 150px;margin-top: 100px;";
+const styleCaixaQuatro = " ";
+
+const fundoUm = "red";
+const fundoDois = "yellow";
+const fundoTres = "blue";
+const fundoQuatro = "green";
 
 const caixaUm = document.createElement("div");
 caixaUm.classList.add("caixa_um");
 caixaUm.style.cssText = styleCaixaUm;
+caixaUm.style.backgroundColor = fundoUm;
 
 const caixaDois = document.createElement("div");
 caixaDois.classList.add("caixa_dois");
 caixaDois.style.cssText = styleCaixaDois;
+caixaDois.style.backgroundColor = fundoDois;
+
 
 const caixaTres = document.createElement("div");
 caixaTres.classList.add("caixa_tres");
 caixaTres.style.cssText = styleCaixaTres;
+caixaTres.style.backgroundColor = fundoTres;
+
 
 const caixaQuatro = document.createElement("div");
 caixaQuatro.classList.add("caixa_quatro");
 caixaQuatro.style.cssText = styleCaixaQuatro;
+caixaQuatro.style.backgroundColor = fundoQuatro;
+
 
 document.body.appendChild(caixaUm);
 document.body.appendChild(caixaDois);
 document.body.appendChild(caixaTres);
 document.body.appendChild(caixaQuatro);
 
+function identificarCaixaClicada(event) {
+  const caixaClicada = event.target;
+  console.log(`Você clicou na ${caixaClicada.classList[0]}`);
+
+  if (caixaClicada === caixaUm) {
+    const tempStyle = caixaUm.style.backgroundColor;
+    caixaUm.style.backgroundColor = caixaDois.style.backgroundColor;
+    caixaDois.style.backgroundColor = tempStyle;
+  } else if (caixaClicada === caixaDois) {
+    const tempStyle = caixaDois.style.backgroundColor;
+    caixaDois.style.backgroundColor = caixaTres.style.backgroundColor;
+    caixaTres.style.backgroundColor = tempStyle;
+  } else if (caixaClicada === caixaTres) {
+    const tempStyle = caixaTres.style.backgroundColor;
+    caixaTres.style.backgroundColor = caixaQuatro.style.backgroundColor;
+    caixaQuatro.style.backgroundColor = tempStyle;
+  } else if (caixaClicada === caixaQuatro) {
+    const tempStyle = caixaQuatro.style.backgroundColor;
+    caixaQuatro.style.backgroundColor = caixaUm.style.backgroundColor;
+    caixaUm.style.backgroundColor = tempStyle;
+  }
+}
+
+const caixas = [caixaUm, caixaDois, caixaTres, caixaQuatro];
+caixas.forEach((caixa) => {
+  caixa.addEventListener("click", identificarCaixaClicada);
+});
